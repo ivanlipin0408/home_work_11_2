28ee84b7f90877a44be0f9f52df9e55c2c887c28
Ivan Lipin <robotlink@yandex.ru>
2025-01-21 20:14:59 +0300

    готов tests, запущен  black

--- scr/decorators.py
+++ scr/decorators.py
@@ -1,34 +1,35 @@
 from datetime import datetime
-from pathlib import Path
 from functools import wraps
-from typing import Callable, Any
+from pathlib import Path
+from typing import Any, Callable
+
 from mypy.util import os_path_join
 
 current_dir = Path(__file__).parent.parent.resolve()
-log_scripts = current_dir/"data"/"mylog.txt"
+log_scripts = current_dir / "data" / "mylog.txt"
 
-def log(filename = None):
+
+def log(filename=None):
     def logging(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            start_time = datetime.now()
             try:
-                result = my_function(*args, **kwargs)
-                log_result = "my_function ok"
+                result = func(*args, **kwargs)
+                log_result = f"{func.__name__} ok. "
 
             except Exception as e:
-                log_result = f"my_function error: {str(e)}. Inputs: ({args}"
+                log_result = f"{func.__name__} error: {str(e)}. Inputs: ({args}"
 
             finally:
-                end_time = datetime.now()
-                log_result += f"Time:{end_time - start_time}"
                 if filename:
-                    with open(filename, 'a') as file:
+                    with open(filename, "a") as file:
                         file.write(log_result + "\n")
                 else:
                     print(log_result)
             return log_result
+
         return wrapper
+
     return logging
 
 
@@ -36,5 +37,5 @@ def log(filename = None):
 def my_function(x, y):
     return x + y
 
-my_function(1, 2)
 
+print(my_function(1, 2))
